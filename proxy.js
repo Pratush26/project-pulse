c2cf3e9dde6613abc46057ea41c068fdc37a6194
Pratush <pratushbarua26@gmail.com>
2025-12-31 21:28:41 +0600

    updated proxy file

--- proxy.js
+++ proxy.js
@@ -1,29 +1,40 @@
 import { getToken } from "next-auth/jwt"
 import { NextResponse } from 'next/server'
 
-const publicRoutes = ['/', '/contact']
 const adminRoutes = ['/manage-users', '/create-project', '/manage-projects', '/register']
 const employeeRoutes = ['/', '/submit-task']
 const clientRoutes = ['/', '/see-projects']
-const authenticateRoutes = ['/', '/dashboard']
 
 export default async function proxy(request) {
     const { pathname } = request.nextUrl
-    if (publicRoutes.includes(pathname)) return NextResponse.next()
 
     const token = await getToken({
         req: request,
         secret: process.env.AUTH_SECRET,
     });
-    if (!token && pathname === "/login") return NextResponse.next()
-    if (token && pathname === "/login") return NextResponse.redirect(new URL('/dashboard', request.url))
     if (!token) return NextResponse.redirect(new URL('/login', request.url))
-    if (authenticateRoutes.includes(pathname)) return NextResponse.next()
-    if (adminRoutes.includes(pathname) && token.role === "admin") return NextResponse.next()
-    if (employeeRoutes.includes(pathname) && token.role === "employee") return NextResponse.next()
-    if (clientRoutes.includes(pathname) && token.role === "client") return NextResponse.next()
-    return NextResponse.redirect(new URL('/', request.url))
+    if (token && pathname === "/login") return NextResponse.redirect(new URL('/dashboard', request.url))
+    if (adminRoutes.includes(pathname) && token?.role === "admin") return NextResponse.next()
+    if (employeeRoutes.includes(pathname) && token?.role === "employee") return NextResponse.next()
+    if (clientRoutes.includes(pathname) && token?.role === "client") return NextResponse.next()
+    return NextResponse.next()
 }
+
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico|css|js)).*)']
-}
+    matcher: [
+        // Admin routes
+        '/manage-users',
+        '/create-project',
+        '/manage-projects',
+        '/register',
+
+        // Employee routes
+        '/submit-task',
+
+        // Client routes
+        '/see-projects',
+
+        // Authenticated routes
+        '/dashboard'
+    ]
+};
